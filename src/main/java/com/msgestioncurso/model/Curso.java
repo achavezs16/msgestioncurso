7f607af058ab061bc9fc2303c96be10641233d68
Matías Suazo <contacto.donmatii@gmail.com>
2025-05-11 16:42:07 -0400

    Se  crean atributos con clase Curso

--- src/main/java/com/msgestioncurso/model/Curso.java
+++ src/main/java/com/msgestioncurso/model/Curso.java
@@ -1,9 +1,19 @@
 package com.msgestioncurso.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Curso {
 
-    
+    private String idCurso;
+    private String nombreCurso;
+    private String descCurso;
+    private int cantParticipantes;
+
 
 }
